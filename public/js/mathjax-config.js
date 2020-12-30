2c044c85c4194de1f87f0cc2a2341894ce5e19dc
PROFESSOR MALCOLM <accounting@professormalcolms.com>
2020-12-30 12:01:52 -0500

    Re-building public in its entirety.

---
+++ public/js/mathjax-config.js
@@ -0,0 +1,22 @@
+// 
+// Plotly does not have this right.
+// See https://github.com/plotly/plotly.js/issues/4563
+//
+window.PlotlyConfig = {MathJaxConfig: 'local'}
+
+// MathJax Configuration
+//
+// v2 to v3 upgrade notes:
+// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
+// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: false,
+    packages: {'[+]': ['noerrors']}
+  },
+  loader: {
+    load: ['[tex]/noerrors']
+  }
+};
