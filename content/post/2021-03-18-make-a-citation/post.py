3c12de0cbffbebddcc62d9206a42a09308345571
PROFESSOR MALCOLM <accounting@professormalcolms.com>
2021-03-18 17:33:14 -0400

    Success on ctns_make. Yay!

---
+++ content/post/2021-03-18-make-a-citation/post.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+def post(target=[], opt=[], url="https://testcite.com/showcase5/"):
+    #
+    if not target:
+        return "Empty target list"
+
+    aTarget = ",".join(target)
+    aOpt    = " ".join(opt)
+    #
+    # data to be sent to api 
+    aData = {'target' : aTarget, 
+             'opt'    : aOpt} 
+    #
+    aResp = requests.post(url = url, data={'payload':json.dumps(aData)})
+    #aResp = requests.post(url = "https://testcite.com/showcase5/", data={'payload':json.dumps(aData)})
+    aSoup = BeautifulSoup(aResp.text, 'html.parser')
+    result = ""
+    #result += str(aSoup.find_all('h3')[0])
+    result += str(aSoup.find_all(class_='ctns-body')[0])
+    result += str(aSoup.find_all(class_='ctns-quiz-container')[0])
+    if "no_buttons" not in opt:
+        result += str(aSoup.find_all(class_='ctns-button-group')[0])
+    result += str(aSoup.find_all(class_='ctns-results')[0])
+    result += str(aSoup.find_all('script')[0])
+    return result
