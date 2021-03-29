4da840a5876a2c413b9da9e905c680f36c3928e2
PROFESSOR MALCOLM <accounting@professormalcolms.com>
2021-03-29 13:45:51 -0400

    Updated post.py throughout.

--- content/algebra/2021-03-24-delete-this/post.py
+++ content/algebra/2021-03-24-delete-this/post.py
@@ -21,8 +21,10 @@ def post(action="ctns", static_dir=STATIC_DIR, image_target=None, target=[], ski
     if not target:
         return "Empty target list"
 
+    marker = str(random.randint(1000,5000))
+
     aTarget  = ",".join(target)
-    aOptCtns = " ".join(opt_ctns) #+ " id='%s'" % "GENERIC_MARKER"
+    aOptCtns = " ".join(opt_ctns) + " id='%s'" % 'GENERIC_MARKER' #"ctns_" + marker #"PYTHON_MARKER"
     aOptDemo = " ".join(opt_demo)
     aOptMake = " ".join(opt_make)
     #
@@ -38,7 +40,7 @@ def post(action="ctns", static_dir=STATIC_DIR, image_target=None, target=[], ski
     if action == 'ctns_make':
         return aResp.text
 
-    marker = "ctns_"+str(random.randint(1000,5000))
+    marker = str(random.randint(1000,5000))
 
     result = ""
     try:
@@ -74,10 +76,16 @@ def post(action="ctns", static_dir=STATIC_DIR, image_target=None, target=[], ski
         name = os.path.basename(script_url_parsed.path).replace(".js", "")
         result += """
 <script type='text/javascript'>
+// These instructions, including the value of %s, are
+// coming from the Python-based script called post.py.
+// The script post.py originates the value of %s, and then
+// passes this value into the system using these two lines
+// below.
+//
 CTNS.QUIZ_SET_ID['%s'] = CTNS.QUIZ_SET_ID['%s'] || [];
 CTNS.QUIZ_SET_ID['%s'].push('%s');
 </script>
-""" % (name, name, name, marker)
+""" % (marker, marker, name, name, name, marker)
 
         image_file = script_url_parsed.path.replace(".js", ".png")
 
