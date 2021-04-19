f887078ec9c1f09764a3fa7ca3e81b3dc1e0893a
PROFESSOR MALCOLM <accounting@professormalcolms.com>
2021-04-19 13:54:33 -0400

    Cleaning up. Removing bad files.

--- public/algebra/screenshot_2.py
+++
@@ -1,3 +0,0 @@
-import chrome_headless_screenshot as C
-
-C.make_screenshot("https://testcite.com/showcase/?target=test-linearQ15-9d-v8", "myscreen.png")
--- public/algebra/ss.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.headless = True
-driver = webdriver.Chrome(options=options, executable_path=r'/Users/mathtutor/bin/chromedriver')
-driver.get("http://google.com/")
-print ("Headless Chrome Initialized")
-driver.quit()
